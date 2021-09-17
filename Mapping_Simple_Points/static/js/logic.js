6e12703048901374a462359572210d012ae3b1e7
Suweatha Sanmuganathan <suweathasanmug@Suweathas-MacBook-Pro.local>
2021-09-17 14:32:44 -0400

    Updating Map for Simple Points with updated logic.js

--- Mapping_Simple_Points/static/js/logic.js
+++ Mapping_Simple_Points/static/js/logic.js
@@ -4,13 +4,28 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      34.0522, -118.2437
     ],
-    zoom: 4
+    zoom: 14
   });
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle marker with a 100m radius over the coordinates
+//L.circle([34.0522, -118.2437], {
+  //radius: 100
+//}).addTo(map);
+
+// Add a circleMarker function to the map 
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300, //number in the function refers to pixels, not metres
+  color: "black",
+  fillColor: "#ffffa1"
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
